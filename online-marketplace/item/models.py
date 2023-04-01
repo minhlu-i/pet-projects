9dc991af998733a1428e34d41ec68460b1cd519f
minhlu-lgtm <quangminhphamlu@gmail.com>
2023-04-01 23:01:18 +0700

    Add: Item & Detail Item

--- online-marketplace/item/models.py
+++ online-marketplace/item/models.py
@@ -1,2 +1,26 @@
+from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    is_sold = models.BooleanField(default=False)
+    category = models.ForeignKey(
+        Category, related_name='items', on_delete=models.CASCADE
+    )
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
