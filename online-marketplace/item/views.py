fac5ddc90964f14cda465cba7061c98d222dc7ae
minhlu-lgtm <quangminhphamlu@gmail.com>
2023-04-06 00:18:48 +0700

    add: add item form

--- online-marketplace/item/views.py
+++ online-marketplace/item/views.py
@@ -1,17 +1,33 @@
-from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
 
+from .forms import NewItemForm
 from .models import Item
 
 
-# Create your views here.
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(
         pk=pk
-    )[
-        0:3
-    ]  # noqa: E501
+    )[0:3]
 
     return render(
         request, "item/detail.html", {"item": item, "related_items": related_items}
     )
+
+
+@login_required
+def new(request):
+    if request.method == "POST":
+        form = NewItemForm(request.POST, request.FILES)
+
+    if form.is_valid():
+        item = form.save(commit=False)
+        item.created_by = request.user
+        item.save()
+
+        return redirect("item:detail", pk=item.pk)
+    else:
+        form = NewItemForm()
+
+    return render(request, "item/form.html", {"form": form, "title": "New Item"})
