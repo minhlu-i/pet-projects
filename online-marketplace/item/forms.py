e74763ba9db2874a76a02cb84a954ae48cc39d1f
minhlu-lgtm <quangminhphamlu@gmail.com>
2023-04-06 23:51:04 +0700

    add: edit & delete item

--- online-marketplace/item/forms.py
+++ online-marketplace/item/forms.py
@@ -23,3 +23,25 @@ class NewItemForm(forms.ModelForm):
             "price": forms.TextInput(attrs={"class": INPUT_CLASSES}),
             "image": forms.FileInput(attrs={"class": INPUT_CLASSES}),
         }
+
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = (
+            "category",
+            "name",
+            "description",
+            "price",
+            "image",
+            "is_sold",
+        )
+
+        widgets = {
+            "category": forms.Select(attrs={"class": INPUT_CLASSES}),
+            "name": forms.TextInput(attrs={"class": INPUT_CLASSES}),
+            "description": forms.Textarea(attrs={"class": INPUT_CLASSES}),
+            "price": forms.TextInput(attrs={"class": INPUT_CLASSES}),
+            "image": forms.FileInput(attrs={"class": INPUT_CLASSES}),
+            # "is_sold": forms.BooleanField(attrs={"class": INPUT_CLASSES}),
+        }
--- online-marketplace/item/views.py
+++ online-marketplace/item/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 
-from .forms import NewItemForm
+from .forms import EditItemForm, NewItemForm
 from .models import Item
 
 
@@ -21,13 +21,36 @@ def new(request):
     if request.method == "POST":
         form = NewItemForm(request.POST, request.FILES)
 
-    if form.is_valid():
-        item = form.save(commit=False)
-        item.created_by = request.user
-        item.save()
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
 
-        return redirect("item:detail", pk=item.pk)
+            return redirect("item:detail", pk=item.pk)
     else:
         form = NewItemForm()
 
     return render(request, "item/form.html", {"form": form, "title": "New Item"})
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    form = EditItemForm(request.POST, request.FILES, instance=item)
+
+    if form.is_valid():
+        form.save()
+
+        return redirect("item:detail", pk=item.pk)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, "item/form.html", {"form": form, "title": "Edit Item"})
+
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+
+    return redirect("dashboard:index")
